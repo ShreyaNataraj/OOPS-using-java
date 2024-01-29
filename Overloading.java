1933a4c07863c7cdaa0ed7e1a94f9730fdea97bd
Shreya N <nshreya094@gmail.com>
2024-01-29 23:51:56 +0530

    overloading example

---
+++ Overloading.java
@@ -0,0 +1,15 @@
+public class Overloading {
+  public static void main(String args[]){
+  Calculator calc = new Calculator();
+  System.out.println(calc.sum(3,4));
+  System.out.println(calc.sum(3,4,5));
+  }
+}
+class Calculator{
+  int sum(int a,int b){
+    return a +b;
+  }
+  int sum(int a, int b, int c){
+    return a+b+c;
+  }
+}
