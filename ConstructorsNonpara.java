0f8b471e79939ab795fba9383118b80321a4dff9
Shreya N <nshreya094@gmail.com>
2024-01-29 22:15:39 +0530

    constructor without parameter

---
+++ ConstructorsNonpara.java
@@ -0,0 +1,11 @@
+public class ConstructorsNonpara {
+  public static void main(String[] args) {
+    Students S1 = new Students();
+  }
+}
+class Students{
+  Students(){
+    System.out.println("Constructor is called");
+  }
+
+}
