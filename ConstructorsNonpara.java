ebb584074b5e7d0fd3911594ad8cd3451f997786
Shreya N <nshreya094@gmail.com>
2024-01-29 22:25:58 +0530

    constructor types

--- ConstructorsNonpara.java
+++ ConstructorsNonpara.java
@@ -1,11 +1,27 @@
 public class ConstructorsNonpara {
   public static void main(String[] args) {
     Students S1 = new Students();
+    Students S2 = new Students("Parinith");
+    System.out.println(S2.name);
+    Students S3 = new Students(55);
+    System.out.println(S3.roll);
+
   }
 }
 class Students{
+  String name ;
+  int roll ;
+  //without parameter
   Students(){
     System.out.println("Constructor is called");
+  }
+  //with parameter
+  Students(String name){
+    this.name = name;
+  }
+  //with parameter
+  Students(int roll){
+    this.roll =roll;
   }
 
 }
